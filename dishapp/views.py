68fadbdca64ba05986937aa9515dfcfae77c0fcf
kirumbastacy25l <minniebooh7@gmail.com>
2025-02-18 13:25:56 +0300

    initial commit

---
+++ dishapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request,'index.html')
+
+
+def gallery(request):
+    return render(request,'gallery.html')
+
+
+
+
+
